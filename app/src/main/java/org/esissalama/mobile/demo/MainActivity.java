901aba96776db1755640c35d2fd369864646b3ad
Meschac Irung <meschacirung@gmail.com>
2022-07-05 17:51:56 +0200

    create rolling dice behavior

--- app/src/main/java/org/esissalama/mobile/demo/MainActivity.java
+++ app/src/main/java/org/esissalama/mobile/demo/MainActivity.java
@@ -1,14 +1,34 @@
 package org.esissalama.mobile.demo;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private ImageView imageView;
+
+    int[] dice_faces = new int[]{
+            R.drawable.dice_1,
+            R.drawable.dice_2,
+            R.drawable.dice_3,
+            R.drawable.dice_4,
+            R.drawable.dice_5,
+            R.drawable.dice_6
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.roll);
+        imageView = findViewById(R.id.dice);
+
+        button.setOnClickListener(v -> {
+            imageView.setImageResource(dice_faces[new Random().nextInt(6)]);
+        });
     }
 }
